dcfd6f5a02119c090d3f817802fba8b980391f32
Saulo <155675783+saulocodes@users.noreply.github.com>
2024-10-03 09:51:13 -0300

    feat: adding tests

--- workspace/littlelemon/restaurant/tests.py
+++ workspace/littlelemon/restaurant/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from restaurant.models import Menu
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from restaurant.serializers import MenuSerializer
+
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title='IceCream', price=80, inventory=100)
+        self.assertEqual(item.title, "IceCream")
+        self.assertEqual(item.price, 80)
+
+class MenuViewTest(APITestCase):
+    def setUp(self):
+        self.item1 = Menu.objects.create(title='Pizza', price=120, inventory=50)
+        self.item2 = Menu.objects.create(title='Pasta', price=100, inventory=60)
+        self.item3 = Menu.objects.create(title='Salad', price=70, inventory=30)
+
+    def test_getall(self):
+        items = Menu.objects.all()
+        serializer = MenuSerializer(items, many=True)
+        self.assertEqual(len(items), 3)
+        self.assertEqual(serializer.data[0]['title'], 'Pizza')
+        self.assertEqual(serializer.data[1]['title'], 'Pasta')
+        self.assertEqual(serializer.data[2]['title'], 'Salad')
+        

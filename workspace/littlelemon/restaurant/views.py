223258fc3ee183b4a68875664bdb38bc545fe4e1
Saulo <155675783+saulocodes@users.noreply.github.com>
2024-10-03 09:19:31 -0300

    feat: adding authentication

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from .serializers import BookingSerializer, MenuSerializer
 from .models import Booking, Menu
 from rest_framework import generics, viewsets
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+@api_view()
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({'msg': 'Hello, Protected World!'})
 
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
@@ -16,5 +26,6 @@ class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
